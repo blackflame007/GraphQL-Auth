0f2681af840d17d3e0c18080ba997793cec53781
Nick L <lesseosnick@gmail.com>
2018-08-03 12:11:40 -0700

    Signup, Login, Logout Mutations and client side setup

--- server/schema/types/user_type.js
+++ server/schema/types/user_type.js
@@ -1,9 +1,10 @@
 const graphql = require('graphql');
-const { GraphQLString, GraphQLObjectType } = graphql;
+const { GraphQLString, GraphQLObjectType, GraphQLID } = graphql;
 
 const UserType = new GraphQLObjectType({
   name: 'UserType',
   fields: {
+    id: { type: GraphQLID },
     email: { type: GraphQLString }
   }
 });
